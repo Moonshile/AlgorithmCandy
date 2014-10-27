667470249461ef16b403e8fa71dd457921fbc5f6
Kaiqiang Dawn <moonshile@foxmail.com>
2014-10-28 07:55:24 +0800

    another version of fast io

---
+++ lib/fread_setbuf.cpp
@@ -0,0 +1,9 @@
+const int SZ = 1<<20;
+struct fastio{
+    char inbuf[SZ];
+    char outbuf[SZ];
+    fastio(){
+        setvbuf(stdin,inbuf,_IOFBF,SZ);
+        setvbuf(stdout,outbuf,_IOFBF,SZ);
+    }
+}io;
