b34788dbab829af7a1197bb8dcd4e9e70cf36d34
Kaiqiang Dawn <moonshile@foxmail.com>
2014-10-30 22:03:07 +0800

    better queap

--- exercise/THU/PA02/queap.cpp
+++ exercise/THU/PA02/queap.cpp
@@ -45,6 +45,58 @@ int Stack::top()
     return _stack[_top - 1];
 }
 
+class MinStack
+{
+public:
+    MinStack();
+	bool empty();
+	void push(int);
+	int pop();
+	int top();
+    int min();
+private:
+    Stack _stack;
+    Stack _min;
+};
+
+MinStack::MinStack()
+{
+}
+
+bool MinStack::empty()
+{
+    return _stack.empty();
+}
+
+void MinStack::push(int value)
+{
+    _stack.push(value);
+    if(_min.empty() || value < _min.top())
+    {
+        _min.push(value);
+    }
+    else
+    {
+        _min.push(_min.top());
+    }
+}
+
+int MinStack::pop()
+{
+    _min.pop();
+    return _stack.pop();
+}
+
+int MinStack::top()
+{
+    return _stack.top();
+}
+
+int MinStack::min()
+{
+    return _min.top();
+}
+
 class Queap
 {
 public:
@@ -53,84 +105,49 @@ public:
 	int dequeap();
 	int min();
 private:
-    Stack _stackMain;
-    Stack _stackHelp;
-    int _size;
-    int _min;
-    void trans(Stack* out, Stack* in, int n);
-    void updateMin();
+    MinStack _stackEn;
+    MinStack _stackDe;
 };
 
 Queap::Queap()
 {
-    _size = 0;
-    _min = 0;
-}
-
-void Queap::trans(Stack* out, Stack* in, int n)
-{
-    for(int i = 0; i < n; i++)
-    {
-        in->push(out->pop());
-    }
-}
-
-void Queap::updateMin()
-{
-    if(_size == 0)
-    {
-        return;
-    }
-    Stack* _out = _stackHelp.empty() ? &_stackMain : &_stackHelp;
-    Stack* _in = _stackHelp.empty() ? &_stackHelp : &_stackMain;
-    int tmp = 0;
-    _min = _out->pop();
-    _in->push(_min);
-    for(int i = 1; i < _size; i++)
-    {
-        tmp = _out->pop();
-        _min = tmp < _min ? tmp : _min;
-        _in->push(tmp);
-    }
-    trans(_in, _out, _size);
 }
 
 void Queap::enqueap(int value)
 {
-    _min = _size == 0 ? value : (value < _min ? value : _min);
-    if(_stackMain.empty())
-    {
-        trans(&_stackHelp, &_stackMain, _size);
-    }
-    _stackMain.push(value);
-    _size++;
+    _stackEn.push(value);
 }
 
 int Queap::dequeap()
 {
-    if(_stackHelp.empty())
+    if(_stackDe.empty())
     {
-        trans(&_stackMain, &_stackHelp, _size);
+        while(!_stackEn.empty())
+        {
+            _stackDe.push(_stackEn.pop());
+        }
     }
-    int res = _stackHelp.pop();
-    _size--;
-    if(res == _min)
-    {
-        updateMin();
-    }
-    return res;
+    return _stackDe.pop();
 }
 
 int Queap::min()
 {
-    return _min;
+    if(!(_stackDe.empty() || _stackEn.empty()))
+    {
+        return _stackEn.min() <= _stackDe.min() ? _stackEn.min() : _stackDe.min();
+    }
+    if(!_stackDe.empty())
+    {
+        return _stackDe.min();
+    }
+    return _stackEn.min();
 }
 
 int main()
 {
     int n, x;
     char ch;
-    Queap queap;
+    Queap* queap = new Queap();
     read_from_stdin();
     next_int(&n);
     for(int i = 0; i < n; i++)
@@ -140,13 +157,13 @@ int main()
         {
         case 'E':
             next_int(&x);
-            queap.enqueap(x);
+            queap->enqueap(x);
             break;
         case 'D':
-            printf("%d\n", queap.dequeap());
+            printf("%d\n", queap->dequeap());
             break;
         default: // M
-            printf("%d\n", queap.min());
+            printf("%d\n", queap->min());
             break;
         }
     }
