baa70a6bf2dd3c73153acf92806b36b9f1a8381b
Kaiqiang Dawn <moonshile@foxmail.com>
2014-11-28 21:54:22 +0800

    update for msort inplace and qsort

--- lib/chaos/msort_inplace.c
+++ lib/chaos/msort_inplace.c
@@ -1,22 +1,73 @@
 #include<stdlib.h>
 #include<stdio.h>
 
-//********************************** merge sort inplace **************************************
-typedef int LIST_TYPE;
-void reverse(LIST_TYPE*, int, int);
-void shift(LIST_TYPE*, int, int, int);
-void merge(LIST_TYPE*, int, int, int, int (*)(const void*, const void*));
-void msort(LIST_TYPE*, int, int, int (*)(const void*, const void*));
+typedef int MSORT_IN_TYPE;
 
+//********************************** merge sort inplace **************************************
+void reverse(MSORT_IN_TYPE*, int, int);
+void shift(MSORT_IN_TYPE*, int, int, int);
+void merge(MSORT_IN_TYPE*, int, int, int, int (*)(const void*, const void*));
+void msort(MSORT_IN_TYPE*, int, int, int (*)(const void*, const void*));
+
+//********************************** merge sort inplace **************************************
+
+// reverse the range [lo, hi) of list
+void reverse(MSORT_IN_TYPE* list, int lo, int hi) {
+    hi--;
+    while(hi > lo) {
+        MSORT_IN_TYPE tmp = list[lo];
+        list[lo] = list[hi];
+        list[hi] = tmp;
+        lo++;
+        hi--;
+    }
+}
+
+// shift the range [lo, hi) of list with count circularly
+void shift(MSORT_IN_TYPE* list, int lo, int hi, int count) {
+    int mid = hi - count;
+    reverse(list, lo, mid);
+    reverse(list, mid, hi);
+    reverse(list, lo, hi);
+}
+
+void merge(MSORT_IN_TYPE* list, int lo, int mid, int hi, int (*cmp)(const void*, const void*)) {
+    int i = lo, j = mid, old_j;
+    while(i < j && j < hi){
+        old_j = j;
+        // i<j must be tested first
+        // find the first i that greater than j
+        while(i < j && cmp(list + i, list + j) <= 0) {
+            i++;
+        }
+        // j<hi must be tested first
+        // find the first j that not lower than i
+        while(j < hi && cmp(list + j, list + i) < 0) {
+            j++;
+        }
+        shift(list, i, j, j - old_j);
+        i += j - old_j;
+    }
+}
+
+// sort range [lo, hi) of list
+void msort(MSORT_IN_TYPE* list, int lo, int hi, int (*cmp)(const void*, const void*)) {
+    if(lo + 1 < hi) {
+        int mid = (lo + hi)>>1;
+        msort(list, lo, mid, cmp);
+        msort(list, mid, hi, cmp);
+        merge(list, lo, mid, hi, cmp);
+    }
+}
 
 //*************************************** test ************************************************
 
 int compare(const void* x, const void* y) {
-    return (int)(*(LIST_TYPE*)x - *(LIST_TYPE*)y);
+    return (int)(*(MSORT_IN_TYPE*)x - *(MSORT_IN_TYPE*)y);
 }
 
 int main() {
-    LIST_TYPE list[][9] = {
+    MSORT_IN_TYPE list[][9] = {
         {1,2,3,4,5,6,7,8,9},
         {9,3,6,4,7,4,2,1,5},
         {9,8,7,6,5,4,3,2,1},
@@ -33,53 +84,3 @@ int main() {
 }
 
 
-//********************************** merge sort inplace **************************************
-
-// reverse the range [lo, hi) of list
-void reverse(LIST_TYPE* list, int lo, int hi) {
-    hi--;
-    while(hi > lo) {
-        LIST_TYPE tmp = list[lo];
-        list[lo] = list[hi];
-        list[hi] = tmp;
-        lo++;
-        hi--;
-    }
-}
-
-// shift the range [lo, hi) of list with count circularly
-void shift(LIST_TYPE* list, int lo, int hi, int count) {
-    int mid = hi - count;
-    reverse(list, lo, mid);
-    reverse(list, mid, hi);
-    reverse(list, lo, hi);
-}
-
-void merge(LIST_TYPE* list, int lo, int mid, int hi, int (*cmp)(const void*, const void*)) {
-    int i = lo, j = mid, old_j;
-    while(i < j && j < hi){
-        old_j = j;
-        // i<j must be tested first
-        // find the first i that greater than j
-        while(i < j && (*cmp)((const void*)(list + i), (const void*)(list + j)) <= 0) {
-            i++;
-        }
-        // j<hi must be tested first
-        // find the first j that not lower than i
-        while(j < hi && (*cmp)((const void*)(list + j), (const void*)(list + i)) < 0) {
-            j++;
-        }
-        shift(list, i, j, j - old_j);
-        i += j - old_j;
-    }
-}
-
-// sort range [lo, hi) of list
-void msort(LIST_TYPE* list, int lo, int hi, int (*cmp)(const void*, const void*)) {
-    if(lo + 1 < hi) {
-        int mid = (lo + hi)>>1;
-        msort(list, lo, mid, cmp);
-        msort(list, mid, hi, cmp);
-        merge(list, lo, mid, hi, cmp);
-    }
-}
--- lib/chaos/qsort.c
+++ lib/chaos/qsort.c
@@ -9,6 +9,66 @@ void sort3(QSORT_TYPE*, int, int, int (*)(const void*, const void*));
 QSORT_TYPE median3(QSORT_TYPE*, int, int, int (*)(const void*, const void*));
 void myQsort(QSORT_TYPE*, int, int, int (*)(const void*, const void*));
 
+//************************************** qsort ***************************************************
+
+#define CUTOFF (3)
+
+// swap the location of a and b
+void swap(QSORT_TYPE* list, int a, int b) {
+    QSORT_TYPE tmp = list[a];
+    list[a] = list[b];
+    list[b] = tmp;
+}
+
+// sort a 3-more-element list, which has a range [lo, hi)
+void sort3(QSORT_TYPE* list, int lo, int hi, int (*cmp)(const void*, const void*)) {
+    int mid = (lo + hi)>>1;
+    hi--;
+    if(cmp(list + lo, list + mid) > 0) {
+        swap(list, lo, mid);
+    }
+    if(cmp(list + lo, list + hi) > 0) {
+        swap(list, lo, hi);
+    }
+    if(cmp(list + mid, list + hi) > 0) {
+        swap(list, mid, hi);
+    }
+}
+
+// select the median of list which has a range [lo, hi)
+// the median is the median of elements at the first, middle, and last location in the list
+QSORT_TYPE median3(QSORT_TYPE* list, int lo, int hi, int (*cmp)(const void*, const void*)) {
+    int mid = (lo + hi)>>1;
+    sort3(list, lo, hi, cmp);
+    swap(list, mid, hi - 2);
+    return list[hi - 2];
+}
+
+// sort a list with range [lo, hi)
+void myQsort(QSORT_TYPE* list, int lo, int hi, int (*cmp)(const void*, const void*)) {
+    int i, j;
+    QSORT_TYPE pivot;
+    if(lo + CUTOFF < hi) {
+        pivot = median3(list, lo, hi, cmp);
+        i = lo;
+        j = hi - 2;
+        for(; ; ) {
+            while(cmp(list + (++i), &pivot) < 0){}
+            while(cmp(list + (--j), &pivot) > 0){}
+            if(i < j) {
+                swap(list, i, j);
+            } else {
+                break;
+            }
+        }
+        swap(list, i, hi - 2);
+        myQsort(list, lo, i, cmp);
+        myQsort(list, i + 1, hi, cmp);
+    } else {
+        sort3(list, lo, hi, cmp);
+    }
+}
+
 //********************************************** test ***********************************************
 
 int compare(const void* x, const void* y) {
@@ -32,63 +92,3 @@ int main() {
     return 0;
 }
 
-
-//************************************** qsort ***************************************************
-
-#define CUTOFF (3)
-
-// swap the location of a and b
-void swap(QSORT_TYPE* list, int a, int b) {
-    QSORT_TYPE tmp = list[a];
-    list[a] = list[b];
-    list[b] = tmp;
-}
-
-// sort a 3-more-element list, which has a range [lo, hi)
-void sort3(QSORT_TYPE* list, int lo, int hi, int (*cmp)(const void*, const void*)) {
-    int mid = (lo + hi)>>1;
-    hi--;
-    if((*cmp)((const void*)(list + lo), (const void*)(list + mid)) > 0) {
-        swap(list, lo, mid);
-    }
-    if((*cmp)((const void*)(list + lo), (const void*)(list + hi)) > 0) {
-        swap(list, lo, hi);
-    }
-    if((*cmp)((const void*)(list + mid), (const void*)(list + hi)) > 0) {
-        swap(list, mid, hi);
-    }
-}
-
-// select the median of list which has a range [lo, hi)
-// the median is the median of elements at the first, middle, and last location in the list
-QSORT_TYPE median3(QSORT_TYPE* list, int lo, int hi, int (*cmp)(const void*, const void*)) {
-    int mid = (lo + hi)>>1;
-    sort3(list, lo, hi, cmp);
-    swap(list, mid, hi - 2);
-    return list[hi - 2];
-}
-
-// sort a list with range [lo, hi)
-void myQsort(QSORT_TYPE* list, int lo, int hi, int (*cmp)(const void*, const void*)) {
-    int i, j;
-    QSORT_TYPE pivot;
-    if(lo + CUTOFF < hi) {
-        pivot = median3(list, lo, hi, cmp);
-        i = lo;
-        j = hi - 2;
-        for(; ; ) {
-            while((*cmp)((const void*)(list + (++i)), (const void*)&pivot) < 0){}
-            while((*cmp)((const void*)(list + (--j)), (const void*)&pivot) > 0){}
-            if(i < j) {
-                swap(list, i, j);
-            } else {
-                break;
-            }
-        }
-        swap(list, i, hi - 2);
-        myQsort(list, lo, i, cmp);
-        myQsort(list, i + 1, hi, cmp);
-    } else {
-        sort3(list, lo, hi, cmp);
-    }
-}
