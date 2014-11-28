a53cbc52950889ce93961cc2d307e16cf106ee60
Kaiqiang Dawn <moonshile@foxmail.com>
2014-11-28 14:12:28 +0800

    some update for binSearch & msort

--- lib/chaos/binary_search.c
+++ lib/chaos/binary_search.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef int BSEARCH_TYPE;
+
 //************************************** binary search **********************************************
-typedef int LIST_TYPE;
-int bin_search(LIST_TYPE*, int, int, LIST_TYPE, int (*)(const void*, const void*));
+int bin_search(BSEARCH_TYPE*, int, int, BSEARCH_TYPE, int (*)(const void*, const void*));
 
 
 //***************************************** test ********************************************************
 
 int compare(const void* a, const void* b){
-    return (int)(*(LIST_TYPE*)a - *(LIST_TYPE*)b);
+    return (int)(*(BSEARCH_TYPE*)a - *(BSEARCH_TYPE*)b);
 }
 
 int main(){
-    LIST_TYPE list[] = {1,2,3,4,5,6,7,8,9};
+    BSEARCH_TYPE list[] = {1,2,3,4,5,6,7,8,9};
     int i;
     for(i = 0; i < 10; i++){
         printf("%d ", bin_search(list, 0, 9, i, &compare));
@@ -26,13 +27,13 @@ int main(){
 
 // search e in list, with range [lo, hi)
 // if e does not exist, then return the greatest index of the element that not greater than e
-int bin_search(LIST_TYPE* list, int lo, int hi, LIST_TYPE e, int (*cmp)(const void*, const void*)){
+int bin_search(BSEARCH_TYPE* list, int lo, int hi, BSEARCH_TYPE e, int (*cmp)(const void*, const void*)){
     // invariant: list[lo] <= e < list[hi]
     while(lo < hi){
         int mi = (hi + lo)>>1;
         // if e < list[mi] then search [lo, mi) else [mi+1, hi)
         // if e== list[mi], then search [mi+1, hi) until [mi+1, mi+1) and break to return mi+1-1
-        if(((*cmp)((const void*)&e, (const void*)(list + mi)) < 0)) {
+        if((cmp(&e, list + mi) < 0)) {
             hi = mi;
         } else {
             lo  = mi + 1;
--- lib/chaos/msort.c
+++ lib/chaos/msort.c
@@ -1,21 +1,67 @@
 #include<stdlib.h>
 #include<stdio.h>
 
-//*********************************** merge sort *****************************************************
-typedef int LIST_TYPE;
-void merge(LIST_TYPE*, LIST_TYPE*, int, int, int, int (*)(const void*, const void*));
-void msort_do(LIST_TYPE*, LIST_TYPE*, int, int, int (*)(const void*, const void*));
-void msort(LIST_TYPE*, int, int, int (*)(const void*, const void*));
+typedef int MSORT_TYPE;
 
+//*********************************** merge sort *****************************************************
+void merge(MSORT_TYPE*, MSORT_TYPE*, int, int, int, int (*)(const void*, const void*));
+void msort_do(MSORT_TYPE*, MSORT_TYPE*, int, int, int (*)(const void*, const void*));
+void msort(MSORT_TYPE*, int, int, int (*)(const void*, const void*));
+
+//*********************************** merge sort *****************************************************
+
+// merge two sub lists that have been sorted
+void merge(MSORT_TYPE* list, MSORT_TYPE* tmp, int lo, int mi, int hi, int (*cmp)(const void*, const void*)){
+    MSORT_TYPE* a = list + lo, *b = tmp + lo, *c = list + mi;
+    int len_b = mi - lo, len_c = hi - mi, i, j, k;
+    // copy b to its temporary location
+    for(i = 0; i < len_b; b[i] = a[i++]);
+    // the test should be j < len_b && k < len_c, but in fact, 
+    // k < len_c is always tested before copy c, so this condition could be omitted
+    for(i = 0, j = 0, k = 0; j < len_b; ) {
+        // the following two if clause could be exchanged,
+        // but will produce extra iterations in this loop
+        
+        // c hasn't been copied, and c[k] is lower than b[j]
+        if(k < len_c && cmp(c + k, b + j) < 0) {
+            a[i++] = c[k++];
+        }
+        // c has been copied, OR b[j] is not greater than c[k]
+        // (1) if c has been copied, then you should copy b only
+        // (2) else if b[j] < c[k], surely you should copy b,
+        //     and if b[j] == c[k], you should copy b first to ensure stability.
+        if(len_c <= k || cmp(b + j, c + k) <= 0) {
+            a[i++] = b[j++];
+        }
+    }
+}
+
+// act merge sort
+void msort_do(MSORT_TYPE* list, MSORT_TYPE* tmp, int lo, int hi, int (*cmp)(const void*, const void*)){
+    if(lo + 1 < hi)
+    {
+        int mid = (lo + hi)>>1;
+        msort_do(list, tmp, lo, mid, cmp);
+        msort_do(list, tmp, mid, hi, cmp);
+        merge(list, tmp, lo, mid, hi, cmp);
+    }
+}
+
+// merge sort driver, sort range [lo, hi) of list
+void msort(MSORT_TYPE* list, int lo, int hi, int (*cmp)(const void*, const void*)){
+    MSORT_TYPE* tmp = (MSORT_TYPE*)malloc(sizeof(MSORT_TYPE)*(hi - lo));
+    msort_do(list, tmp, lo, hi, cmp);
+    free(tmp);
+}
 
 //************************************************* test ****************************************
 
 int compare(const void* x, const void* y){
-    return (int)(*(LIST_TYPE*)x - *(LIST_TYPE*)y);
+    return (int)(*(MSORT_TYPE*)x - *(MSORT_TYPE*)y);
 }
 
 int main(){
-    LIST_TYPE list[][9] = {
+    MSORT_TYPE list[][9] = {
         {1,2,3,4,5,6,7,8,9},
         {9,3,6,4,7,4,2,1,5},
         {9,8,7,6,5,4,3,2,1},
@@ -32,49 +78,3 @@ int main(){
     return 0;
 }
 
-
-//*********************************** merge sort *****************************************************
-
-// merge two sub lists that have been sorted
-void merge(LIST_TYPE* list, LIST_TYPE* tmp, int lo, int mi, int hi, int (*cmp)(const void*, const void*)){
-    LIST_TYPE* a = list + lo, *b = tmp + lo, *c = list + mi;
-    int len_b = mi - lo, len_c = hi - mi, i, j, k;
-    // copy b to its temporary location
-    for(i = 0; i < len_b; b[i] = a[i++]);
-    // the test should be j < len_b && k < len_c, but in fact, 
-    // k < len_c is always tested before copy c, so this condition could be omitted
-    for(i = 0, j = 0, k = 0; j < len_b; ) {
-        // the following two if clause could be exchanged,
-        // but will produce extra iterations in this loop
-        
-        // c hasn't been copied, and c[k] is lower than b[j]
-        if(k < len_c && (*cmp)((const void*)(c + k), (const void*)(b + j)) < 0) {
-            a[i++] = c[k++];
-        }
-        // c has been copied, OR b[j] is not greater than c[k]
-        // (1) if c has been copied, then you should copy b only
-        // (2) else if b[j] < c[k], surely you should copy b,
-        //     and if b[j] == c[k], you should copy b first to ensure stability.
-        if(len_c <= k || (*cmp)((const void*)(b + j), (const void*)(c + k)) <= 0) {
-            a[i++] = b[j++];
-        }
-    }
-}
-
-// act merge sort
-void msort_do(LIST_TYPE* list, LIST_TYPE* tmp, int lo, int hi, int (*cmp)(const void*, const void*)){
-    if(lo + 1 < hi)
-    {
-        int mid = (lo + hi)>>1;
-        msort_do(list, tmp, lo, mid, cmp);
-        msort_do(list, tmp, mid, hi, cmp);
-        merge(list, tmp, lo, mid, hi, cmp);
-    }
-}
-
-// merge sort driver, sort range [lo, hi) of list
-void msort(LIST_TYPE* list, int lo, int hi, int (*cmp)(const void*, const void*)){
-    LIST_TYPE* tmp = (LIST_TYPE*)malloc(sizeof(LIST_TYPE)*(hi - lo));
-    msort_do(list, tmp, lo, hi, cmp);
-    free(tmp);
-}
