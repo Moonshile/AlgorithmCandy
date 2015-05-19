d5f65f1b227384a8d7eba223597a3b4ee5673710
Kaiqiang Dawn <moonshile@foxmail.com>
2015-05-19 20:05:27 +0800

    #40

--- leetcode/40.cpp
+++ leetcode/40.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
         sort(candidates.begin(), candidates.end());
         int level = 0, i = 0, cur = 0;
         vector<vector<int>> res;
         vector<int> c, level_base;
         while (level >= 0) {
             // forwarding
-            for(; cur < target; ++level) {
+            for(; cur < target && i < candidates.size(); ++level, ++i) {
                 level_base.push_back(i);
                 c.push_back(candidates[i]);
                 cur += candidates[i];
@@ -20,7 +20,13 @@ public:
             // backwarding
             do {
                 --level;
-                i = level >= 0 ? level_base.back() + 1 : -1;
+                if (level >= 0) {
+                    i = level_base.back();
+                    // locate to next different i to avoid duplicates
+                    for (int old_i = i; i < candidates.size() && candidates[i] == candidates[old_i]; ++i) {}
+                } else {
+                    i = -1;
+                }
                 cur -= c.back();
                 c.pop_back();
                 level_base.pop_back();
