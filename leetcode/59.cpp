13e0b6b91a5a3d2d60776fb4f98f267ef1422150
Kaiqiang Dawn <moonshile@foxmail.com>
2015-05-21 21:46:25 +0800

    #59 updated, support mXn

--- leetcode/59.cpp
+++ leetcode/59.cpp
@@ -1,33 +1,38 @@
 class Solution {
-public:
-    vector<vector<int>> generateMatrix(int n) {
+private:
+    vector<vector<int>> generateMatrixDo(int m, int n) {
         vector<vector<int>> res;
-        for (int i = 0; i < n; ++i) {
+        for (int i = 0; i < m; ++i) {
             vector<int> tmp(n, 0);
             res.push_back(tmp);
         }
-        int k = (n + 1)>>1, num = 1;
-        for (int i = 0, j = 0; i < k && j < k; ++i, ++j) {
-            for (int jj = j; jj < n - 1 - j; ++jj) {
+        int ki = (m + 1)>>1, kj = (n + 1)>>1, mm = m - 1, nn = n - 1, num = 1;
+        for (int i = 0, j = 0; i < ki && j < kj; ++i, ++j) {
+            for (int jj = j; jj < nn - j; ++jj) {
                 res[i][jj] = num++;
             }
-            for (int ii = i; ii < n - 1 - i; ++ii) {
-                res[ii][n - 1 - j] = num++;
+            for (int ii = i; ii < mm - i; ++ii) {
+                res[ii][nn - j] = num++;
             }
-            if (i != n - 1 - i) {
-                for (int jj = n - 1 - j; jj > j; --jj) {
-                    res[n - 1 - i][jj] = num++;
+            if (i != mm - i) {
+                for (int jj = nn - j; jj > j; --jj) {
+                    res[mm - i][jj] = num++;
                 }
             }
-            if (j != n - 1 - j) {
-                for (int ii = n - 1 - i; ii > i; --ii) {
+            if (j != nn - j) {
+                for (int ii = mm - i; ii > i; --ii) {
                     res[ii][j] = num++;
                 }
             }
-            if (i == n - 1 - i || j == n - 1 - j) {
-                res[n - 1 - i][n - 1 - j] = num++;
+            if (i == mm - i || j == nn - j) {
+                res[mm - i][nn - j] = num++;
             }
         }
         return res;
+    }
+    
+public:
+    vector<vector<int>> generateMatrix(int n) {
+        return generateMatrixDo(n, n);
     }
 };
