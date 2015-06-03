d941a072676b2d90ca5a4ad311195b191cf98ab9
Kaiqiang Dawn <moonshile@foxmail.com>
2015-06-03 17:01:25 +0800

    #84

---
+++ leetcode/84.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& height) {
+        stack<int> ascend;
+        int res = 0, i = 0;
+        height.push_back(0);
+        while (i < height.size()) {
+            if (ascend.empty() || height[ascend.top()] <= height[i]) {
+                ascend.push(i++);
+            } else {
+                int j = ascend.top();
+                ascend.pop();
+                res = max(res, height[j]*(ascend.empty() ? i : i - ascend.top() - 1));
+            }
+        }
+        return res;
+    }
+};
