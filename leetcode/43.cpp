dfd010433e940d166e38e394af4e872a77eb2d93
Kaiqiang Duan <moonshile@foxmail.com>
2015-06-16 08:41:22 +0800

    #43 string multiply: class

--- leetcode/43.cpp
+++ leetcode/43.cpp
@@ -1,100 +1,166 @@
-class Solution {
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <stdexcept>
+#include <cctype>
+#include <algorithm>
+
+using namespace std;
+
+class not_valid_number: public runtime_error {
+public:
+    not_valid_number(const string &s): runtime_error(s + " is not a valid number") {}
+};
+
+class wrong_scalar_range: public runtime_error {
+public:
+    wrong_scalar_range(long long scalar):
+      runtime_error(scalar + "is not in range [0, 999999999]") { }
+};
+
+class BigInt {
+    friend const BigInt operator+(const BigInt &bi1, const BigInt &bi2);
+    friend const BigInt scalar_multiply(const BigInt &bi, long long scalar, int base);
+    friend const BigInt operator*(const BigInt &bi1, const BigInt &bi2);
 private:
-    struct BigInt {
-        vector<long long> digits;
-        const long long radix = 1000000000;
-        
-        BigInt() {}
-        
-        BigInt(string num) {
-            string zeros(num.size()%9 == 0 ? 0 : 9 - num.size()%9, '0');
-            num = zeros + num;
-            for (int i = static_cast<int>(num.size()) - 9; i >= 0; i -= 9) {
-                long long cur = 0;
-                for (int j = 0; j < 9; ++j) {
-                    cur = cur*10 + num[i + j] - '0';
-                }
-                digits.push_back(cur);
-            }
+    // store digits from lower to higher
+    vector<long long> digits;
+    bool positive;
+
+    static const long long radix() {
+        return 1000000000;
+    }
+
+    static const int max_unit() {
+        return 9;
+    }
+
+    void minify() {
+        while (digits.size() > 1 && digits.back() == 0) {
+            digits.pop_back();
         }
-        
-        string toString() const {
-            stringstream ss;
-            for (int i = static_cast<int>(digits.size()) - 1; i >= 0; --i) {
-                stringstream tmp;
-                tmp << digits[i];
-                string d = tmp.str(), zeros(9 - d.size(), '0');
-                ss << (i == static_cast<int>(digits.size()) - 1 ? "" : zeros) << d;
-            }
-            return ss.str() == "" ? "0" : ss.str();
-        }
-        
-        const BigInt operator+(const BigInt &bi) const {
-            BigInt res;
-            long long cur = 0, len1 = digits.size(), len2 = bi.digits.size(), len = min(len1, len2);
-            const BigInt &greater = len1 > len2 ? *this : bi, &lower = len1 > len2 ? bi : *this;
-            for (int i = 0; i < len; ++i) {
-                cur += greater.digits[i] + lower.digits[i];
-                res.digits.push_back(cur%radix);
-                cur /= radix;
-            }
-            for (unsigned int i = len; i < greater.digits.size(); ++i) {
-                cur += greater.digits[i];
-                res.digits.push_back(cur%radix);
-                cur /= radix;
-            }
-            if (cur) {
-                res.digits.push_back(cur);
-            }
-            return res;
-        }
-        
-        const BigInt multiply_num(long long n) const {
-            BigInt res;
-            if (n == 0) {
-                return res;
+    }
+
+public:
+    explicit BigInt(const string &num): digits() {
+        if ((num.size() == 1 && !isdigit(num[0])) ||
+            (num.size() > 1 && !isdigit(num[0]) && num[0] != '-' && num[0] != '+')) {
+            throw not_valid_number(num);
+        } else if (num.size() == 0) {
+            digits.push_back(0);
+        } else {
+            positive = num[0] == '-' ? false : true;
+            int end = isdigit(num[0]) ? 0 : 1;
+            while (isspace(num[end])) {
+                ++end;
             }
             long long cur = 0;
-            for (unsigned int i = 0; i < digits.size(); ++i) {
-                cur += n*digits[i];
-                res.digits.push_back(cur%radix);
-                cur /= radix;
-            }
-            if (cur) {
-                res.digits.push_back(cur);
-            }
-            return res;
-        }
-        
-        const BigInt upgrade() {
-            if (digits.size() > 0) {
-                digits.insert(digits.begin(), 0);
+            for (int i = static_cast<int>(num.size()) - 1, j = 1; i >= end; --i) {
+                if (isdigit(num[i])) {
+                    cur += (num[i] - '0')*j;
+                    j *= 10;
+                    if (j == radix() || i == end) {
+                        j = 1;
+                        digits.push_back(cur);
+                        cur = 0;
+                    }
+                } else {
+                    throw not_valid_number(num);
                 }
-            return *this;
-        }
-        
-        void copy(const BigInt &bi) {
-            digits = bi.digits;
-        }
-        
-        const BigInt operator*(const BigInt &bi) const {
-            BigInt res;
-            if (digits.size() == 0 || bi.digits.size() == 0) {
-                return res;
             }
-            int i = static_cast<int>(bi.digits.size()) - 1;
-            res.copy(this->multiply_num(bi.digits[i]));
-            for (--i; i >= 0; --i) {
-                res.upgrade();
-                res.copy(res + this->multiply_num(bi.digits[i]));
-            }
-            return res;
         }
-    };
-    
+    }
+
+    BigInt(): BigInt("0") { }
+
+    BigInt(const vector<long long> &ds, bool pos=true): digits(ds), positive(pos) {
+        for (auto d: ds) {
+            if (d >= radix() || d < 0) {
+                throw not_valid_number("The array");
+            }
+        }
+    }
+
+    BigInt(const BigInt &big_int, bool pos=true): BigInt(big_int.digits, pos) { }
+
+    string toString() const {
+        stringstream ss, tmp;
+        if (!positive) {
+            ss << "-";
+        }
+        for (int n = static_cast<int>(digits.size()) - 1, i = n; i >= 0; --i) {
+            tmp.str("");
+            tmp << digits[i];
+            string d = tmp.str(), zeros(max_unit() - d.size(), '0');
+            ss << (i == n ? "" : zeros) << d;
+        }
+        return ss.str() == "" ? "0" : ss.str();
+    }
+};
+
+const BigInt operator+(const BigInt &bi1, const BigInt &bi2) {
+    // TODO handle negative inputs
+    BigInt res(vector<long long>(max(bi1.digits.size(), bi2.digits.size()), 0));
+    long long carry = 0;
+    for (unsigned int i = 0; i < res.digits.size(); ++i) {
+        if (i < bi1.digits.size()) {
+            res.digits[i] += bi1.digits[i];
+        }
+        if (i < bi2.digits.size()) {
+            res.digits[i] += bi2.digits[i];
+        }
+        res.digits[i] += carry;
+        carry = res.digits[i]/BigInt::radix();
+        res.digits[i] %= BigInt::radix();
+    }
+    if (carry) {
+        res.digits.push_back(carry);
+    }
+    res.minify();
+    return res;
+}
+
+const BigInt scalar_multiply(const BigInt &bi, long long scalar, int base) {
+    if (abs(scalar) >= BigInt::radix()) {
+        throw wrong_scalar_range(scalar);
+    }
+    bool positive = (bi.positive && scalar >= 0) || (!bi.positive && scalar < 0);
+    scalar = abs(scalar);
+    BigInt res(vector<long long>(bi.digits.size() + base, 0), positive);
+    long long carry = 0;
+    for (unsigned int i = base; i < res.digits.size(); ++i) {
+        res.digits[i] += carry + scalar*bi.digits[i - base];
+        carry = res.digits[i]/BigInt::radix();
+        res.digits[i] %= BigInt::radix();
+    }
+    if (carry) {
+        res.digits.push_back(carry);
+    }
+    res.minify();
+    return res;
+}
+
+const BigInt operator*(const BigInt &bi1, const BigInt &bi2) {
+    BigInt res;
+    for (unsigned int i = 0; i < bi2.digits.size(); ++i) {
+        res = res + scalar_multiply(bi1, bi2.digits[i], i);
+    }
+    res.minify();
+    res.positive = !(bi1.positive ^ bi2.positive);
+    return res;
+}
+
+class Solution {
 public:
     string multiply(string num1, string num2) {
         BigInt n1(num1), n2(num2);
         return (n1*n2).toString();
     }
 };
+
+int main() {
+    Solution s;
+    cout << s.multiply("-6913259244", "-71103343") << endl;
+}
