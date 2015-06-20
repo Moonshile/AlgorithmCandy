c4e01baed6853ed30205921894cb9244b895575b
Kaiqiang Dawn <moonshile@foxmail.com>
2015-06-20 09:51:47 +0800

    #136

---
+++ leetcode/136.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int res = 0;
+        for (int i: nums) {
+            res ^= i;
+        }
+        return res;
+    }
+};
