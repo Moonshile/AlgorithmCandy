305e62f976f4fb98fe6472cff5edf476c11c6afb
Kaiqiang Dawn <moonshile@foxmail.com>
2015-06-27 13:51:28 +0800

    #219

--- leetcode/219.cpp
+++ leetcode/219.cpp
@@ -2,14 +2,10 @@ class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
         unordered_set<int> m;
-        for (int i = 0; i <= k && i < nums.size(); ++i) {
-            if (m.count(nums[i]) > 0) {
-                return true;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (i > k) {
+                m.erase(nums[i - k - 1]);
             }
-            m.insert(nums[i]);
-        }
-        for (int i = k + 1; i < nums.size(); ++i) {
-            m.erase(nums[i - k - 1]);
             if (m.count(nums[i]) > 0) {
                 return true;
             }
