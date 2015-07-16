40652f92e1a4fda7b3de0b4ec11f2dc1fbb91d06
Kaiqiang Dawn <moonshile@foxmail.com>
2015-07-16 18:53:55 +0800

    #238  refine

--- leetcode/238.cpp
+++ leetcode/238.cpp
@@ -2,13 +2,16 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         vector<int> res(nums.size(), 1);
+        if (nums.empty()) {
+            return res;
+        }
         int tmp = 1;
-        for (int i = 0; i < static_cast<int>(nums.size()) - 1; ++i) {
+        for (size_t i = 0; i != nums.size() - 1; ++i) {
             tmp *= nums[i];
             res[i + 1] *= tmp;
         }
         tmp = 1;
-        for (int i = static_cast<int>(nums.size()) - 1; i > 0; --i) {
+        for (size_t i = nums.size() - 1; i != 0; --i) {
             tmp *= nums[i];
             res[i - 1] *= tmp;
         }
